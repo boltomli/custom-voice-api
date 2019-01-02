93609e5b8cd3800647e62129c1b4b722fb5b52be
Song Li <songl@outlook.com>
2019-01-02 18:58:55 +0800

    Get endpoints

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,6 +15,12 @@ export class HomePage {
   token: string;
   modelUrl: string;
   models: any;
+  endpointUrl: string;
+  endpoints: any;
+  endpoint: string;
+  locale: string;
+  gender: string;
+  name: string;
   voice: string;
   text: string;
   testUrl: string;
@@ -31,7 +37,7 @@ export class HomePage {
         this.storage.get('key').then((key) => {
           this.key = key;
           if (key) {
-            this.getModels();
+            this.getEndpoints();
           } else {
             this.toastCtrl.create({
               message: 'Empty key',
@@ -80,10 +86,50 @@ export class HomePage {
     this.voice = model.id;
   }
 
+  getEndpoints() {
+    this.tokenUrl = `https://${this.region}.api.cognitive.microsoft.com/sts/v1.0/issueToken`;
+    this.http.post(this.tokenUrl, '', {
+      headers: {'Ocp-Apim-Subscription-Key': this.key},
+      responseType: 'text'
+    }).subscribe((token) => {
+      this.token = token;
+      this.endpointUrl = `https://${this.region}.cris.ai/api/texttospeech/v2.0/endpoints`;
+      this.http.get(this.endpointUrl, {
+        headers: {'Ocp-Apim-Subscription-Key': this.key},
+        responseType: 'json'
+      }).subscribe((endpoints) => {
+        this.endpoints = endpoints;
+      }, (err: Error) => {
+        this.toastCtrl.create({
+          message: 'No valid endpoint?\n' + err.message,
+          duration: 1000
+        }).then((toast) => {
+          toast.present();
+        });
+      });
+    }, (err: Error) => {
+      this.toastCtrl.create({
+        message: 'Wrong key or region?\n' + err.message,
+        duration: 1000
+      }).then((toast) => {
+        toast.present();
+      });
+    });
+  }
+
+  selectEndpoint(endpoint: any) {
+    if (endpoint.status === 'Succeeded') {
+      this.endpoint = endpoint.endpointUrls.Ssml;
+      this.locale = endpoint.locale;
+      this.gender = endpoint.models[0].properties.gender;
+      this.name = endpoint.name;
+    }
+  }
+
   saveSettings() {
     this.storage.set('region', this.region).then(() => {
       this.storage.set('key', this.key).then(() => {
-        this.getModels();
+        this.getEndpoints();
       });
     });
   }
