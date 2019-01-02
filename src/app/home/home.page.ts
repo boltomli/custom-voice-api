c4a876c7c90815e4473a9c6972fda851d958c42c
Song Li <songl@outlook.com>
2019-01-02 18:31:58 +0800

    Get models

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,12 +13,11 @@ export class HomePage {
   key: string;
   tokenUrl: string;
   token: string;
+  modelUrl: string;
+  models: any;
   voice: string;
-
-  voice_items = [
-    'test 1',
-    'test 2'
-  ];
+  text: string;
+  testUrl: string;
 
   constructor(
     private platform: Platform,
@@ -31,30 +30,43 @@ export class HomePage {
         this.region = region ? region : 'westus';
         this.storage.get('key').then((key) => {
           this.key = key;
-          this.validateKey();
+          if (key) {
+            this.getModels();
+          } else {
+            this.toastCtrl.create({
+              message: 'Empty key',
+              duration: 1000
+            }).then((toast) => {
+              toast.present();
+            });
+          }
         });
       });
     });
   }
 
-  voiceSelected() {
-    console.log('Selected ', this.voice);
-  }
-
-  validateKey() {
+  getModels() {
     this.tokenUrl = `https://${this.region}.api.cognitive.microsoft.com/sts/v1.0/issueToken`;
     this.http.post(this.tokenUrl, '', {
       headers: {'Ocp-Apim-Subscription-Key': this.key},
       responseType: 'text'
     }).subscribe((token) => {
       this.token = token;
-      this.toastCtrl.create({
-        message: 'Key validated against Speech Services.',
-        duration: 1000
-      }).then((toast) => {
-        toast.present();
+      this.modelUrl = `https://${this.region}.cris.ai/api/texttospeech/v2.0/models`;
+      this.http.get(this.modelUrl, {
+        headers: {'Ocp-Apim-Subscription-Key': this.key},
+        responseType: 'json'
+      }).subscribe((models) => {
+        this.models = models;
+      }, (err: Error) => {
+        this.toastCtrl.create({
+          message: 'No valid model?\n' + err.message,
+          duration: 1000
+        }).then((toast) => {
+          toast.present();
+        });
       });
-    }, (err) => {
+    }, (err: Error) => {
       this.toastCtrl.create({
         message: 'Wrong key or region?\n' + err.message,
         duration: 1000
@@ -64,10 +76,14 @@ export class HomePage {
     });
   }
 
+  selectModel(model: any) {
+    this.voice = model.id;
+  }
+
   saveSettings() {
     this.storage.set('region', this.region).then(() => {
       this.storage.set('key', this.key).then(() => {
-        this.validateKey();
+        this.getModels();
       });
     });
   }
