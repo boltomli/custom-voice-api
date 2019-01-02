188299d59421d713d19b09960d2f9b9d86397ac7
Song Li <songl@outlook.com>
2019-01-02 14:48:36 +0800

    Set region and key

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  region: string;
+  key: string;
+  tokenUrl: string;
+  token: string;
   voice: string;
 
   voice_items = [
@@ -13,7 +19,39 @@ export class HomePage {
     'test 2'
   ];
 
+  constructor(
+    private platform: Platform,
+    private storage: Storage
+  ) {
+    this.platform.ready().then(() => {
+      this.storage.get('region').then((region) => {
+        this.region = region ? region : 'westus';
+        this.tokenUrl = `https://${this.region}.api.cognitive.microsoft.com/sts/v1.0/issueToken`;
+        this.storage.get('key').then((key) => {
+          this.key = key;
+          this.validateKey();
+        });
+      });
+    });
+  }
+
   voiceSelected() {
     console.log('Selected ', this.voice);
+  }
+
+  validateKey() {
+    if (this.key) {
+      this.token = 'placeholder';
+    } else {
+      this.token = '';
+    }
+  }
+
+  saveSettings() {
+    this.storage.set('region', this.region).then(() => {
+      this.storage.set('key', this.key).then(() => {
+        this.validateKey();
+      });
+    });
   }
 }
